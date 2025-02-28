5748c0913fc75704654412890ba2f777ab333045
kjxbyz <kjxbyz888@outlook.com>
2025-02-28 19:20:13 +0800

    fix: remove GitHub button

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -162,17 +162,17 @@ export default function Home({
           className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0"
           style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}
         >
-          <Link
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 dark:bg-black dark:text-white/80"
-            href="https://github.com/insco-inc"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Github />
-            <p>
-              <span className="sm:inline-block">Star on GitHub</span>
-            </p>
-          </Link>
+          {/*<Link*/}
+          {/*  className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 dark:bg-black dark:text-white/80"*/}
+          {/*  href="https://github.com/insco-inc"*/}
+          {/*  target="_blank"*/}
+          {/*  rel="noopener noreferrer"*/}
+          {/*>*/}
+          {/*  <Github />*/}
+          {/*  <p>*/}
+          {/*    <span className="sm:inline-block">Star on GitHub</span>*/}
+          {/*  </p>*/}
+          {/*</Link>*/}
           <Link
             className="flex min-w-32 max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-5 py-2 text-sm text-gray-600 shadow-md transition-colors hover:border-gray-800 dark:bg-black dark:text-white/80"
             href="support"
